1f7f1c54093b66cdb11d8bc75d4f70c7d59f91dd
Lou1sM <lmahonology@gmail.com>
2025-03-19 17:03:01 +0800

    remove unneeded dependency

--- LCC/utils.py
+++ LCC/utils.py
@@ -1,49 +1,6 @@
 import numpy as np
 import pandas as pd
-from line_profiler import LineProfiler
 
-
-def profile_aggregate(func):
-    """
-    Decorator that accumulates timing stats across multiple function calls
-    and prints results only at the end of the program
-    """
-    # Create a single profiler instance that persists across calls
-    profiler = LineProfiler()
-    wrapped = profiler(func)
-
-    def wrapper(*args, **kwargs):
-        return wrapped(*args, **kwargs)
-
-    # Store the profiler so we can access it later
-    wrapper.profiler = profiler
-    return wrapper
-
-def profile_lines(func):
-    """
-    Decorator to profile specific lines within a function
-    """
-    def wrapper(*args, **kwargs):
-        profiler = LineProfiler()
-        profiler_wrapper = profiler(func)
-        result = profiler_wrapper(*args, **kwargs)
-
-        print("\n=== Line-by-line profiling ===")
-        profiler.print_stats()
-
-        return result
-    return wrapper
-
-def print_stats(func):
-    """Call this after all executions to see accumulated stats"""
-    if hasattr(func, 'profiler'):
-        func.profiler.print_stats()
-    else:
-        print("No profiler found on function")
-
-def make_alls_df(df):
-    inner_alls_dfs = [df.loc[k,'all'] for k in df.index.levels[0]]
-    return pd.concat(inner_alls_dfs,keys=df.index.levels[0],axis=1).T
 
 def opt_cost_from_counts(counts):
     if len(counts)==1:
@@ -60,4 +17,3 @@ def opt_cost_from_counts(counts):
 def opt_cost_from_discrete_seq(t):
     v, counts = np.unique(list(t), return_counts=True)
     return opt_cost_from_counts(counts)
-
